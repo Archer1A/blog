3f6c8e2ba480d26a2e20f9bf8aa4bd2fd77a4ab3
Vic.wong <985695123@qq.com>
2019-01-09 21:11:53 +0800

    修复Thymeleaf 无法正常加载css样式问题

--- src/main/java/com/vic/blog/BlogApplication.java
+++ src/main/java/com/vic/blog/BlogApplication.java
@@ -7,17 +7,17 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class BlogApplication extends WebMvcConfigurationSupport {
+public class BlogApplication /*extends WebMvcConfigurationSupport */{
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
-/*    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX);
-        super.addResourceHandlers(registry);
-
-    }*/
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//        //super.addResourceHandlers(registry);
+//
+//    }
 }
 

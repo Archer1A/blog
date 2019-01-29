9d3c8760508be517477ee5250d69bb61fe3f9b9c
Vic.wong <985695123@qq.com>
2019-01-29 16:31:45 +0800

    新增拦截器

--- src/main/java/com/vic/blog/config/WebMvcConfig.java
+++ src/main/java/com/vic/blog/config/WebMvcConfig.java
@@ -1,15 +1,35 @@
 package com.vic.blog.config;
 
+import com.vic.blog.interceptor.LoginTimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Autowired
+    LoginTimeInterceptor loginTimeInterceptor;
+
+    /**
+     * 配置静态资源访问路径
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 配置拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginTimeInterceptor).addPathPatterns("/**").excludePathPatterns("/login","/index");
+
     }
 }
--- src/main/java/com/vic/blog/interceptor/LoginTimeInterceptor.java
+++ src/main/java/com/vic/blog/interceptor/LoginTimeInterceptor.java
@@ -1,50 +1,68 @@
 package com.vic.blog.interceptor;
 
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Calendar;
 
 /**
  * 创建拦截器
  */
+@Component
 public class LoginTimeInterceptor extends HandlerInterceptorAdapter {
 
-    private int startTime;
-
-    private int endTime;
-
-    public void setStartTime(int startTime) {
-        this.startTime = startTime;
-    }
-
-    public void setEndTime(int endTime) {
-        this.endTime = endTime;
-    }
-
+    /**
+     *  该方法在控制器执行前调用，可用于权限控制，登录验证等
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws ServletException
+     * @throws IOException
+     */
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,Object handler) throws ServletException, IOException {
         System.out.println("执行preHandle方法-->01");
-        Calendar calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        if (startTime<=hour &&hour<endTime){
+        HttpSession session = request.getSession();
+        String user = (String) session.getAttribute("userName");
+        if (user != null){
             return true; //通过拦截器，继续执行
         }else {
-            request.setAttribute("msg", "非登录时段");
-            request.getRequestDispatcher("/login").forward(request,response);
+            System.out.println(request.getContextPath()+"/login");
+            response.sendRedirect(request.getContextPath()+"/login");
             return false;
         }
     }
+
+    /**
+     * 后端控制器执行完成后调用
+     * @param request
+     * @param response
+     * @param handle
+     * @param modelAndView
+     * @throws Exception
+     */
     public void postHandle(HttpServletRequest request,
                       HttpServletResponse response, Object handle, ModelAndView modelAndView) throws Exception {
         System.out.println("执行postHandle方法-->02");
         super.postHandle(request,response,handle,modelAndView);
     }
 
+    /**
+     * 整个请求完成后调用
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
     public void afterCompletion(HttpServletRequest request,HttpServletResponse response,Object handler,Exception ex) throws Exception {
         System.out.println("执行afterCompletion方法-->003");
         super.afterCompletion(request,response,handler,ex);
